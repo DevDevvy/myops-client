88e441a2e8b1395268820f33b31ac8982a021364
Randall Thomas <randallthomasmusic@gmail.com>
2022-08-08 14:31:16 -0700

    https redirect added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import { App } from "./components/App.js"
+import HttpsRedirect from 'react-https-redirect'
 import "./index.css"
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -11,7 +12,9 @@ import '@fontsource/roboto/700.css';
 ReactDOM.render(
     <React.StrictMode>
         <Router>
-            <App />
+            <HttpsRedirect>
+                <App />
+            </HttpsRedirect>
         </Router>
     </React.StrictMode>,
     document.getElementById("root")
